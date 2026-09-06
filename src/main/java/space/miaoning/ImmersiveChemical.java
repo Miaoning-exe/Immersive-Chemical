6dd62e0aa301e8db883f2d28dc7e03ab9a4ad476
miaoning <miaoning.exe@gmail.com>
2026-09-06 23:48:39 +0800

    build: config dependencies.

---
+++ src/main/java/space/miaoning/ImmersiveChemical.java
@@ -0,0 +1,11 @@
+package space.miaoning;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(ImmersiveChemical.MODID)
+public class ImmersiveChemical {
+    public static final String MODID = "immersive_chemical";
+    public static final Logger LOGGER = LogUtils.getLogger();
+}
